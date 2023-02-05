ddc68ccfb1632e50f8ee4cc5f3263777e734e406
ShrutiSontakke <124530293+ShrutiSontakke@users.noreply.github.com>
2023-02-05 16:25:57 -0500

    Add files via upload

---
+++ HW_1.R
@@ -0,0 +1,33 @@
+---
+  title: "Vector_And_Matrix"
+author: "ss4230@scarletmail.rutgers.edu"
+date: "02/05/2023"
+output: html_document
+---
+library(readr)
+Bumpus_sparrows <- read_csv("C:\Users\Shruti Sontakke\Downloads\Bumpus_sparrows.csv")
+Bumpus_sparrows
+sparr <- Bumpus_sparrows
+sparr
+
+#Separating two matrices as survived and not survived
+survived <- subset(sparr, Survivorship == 'S')
+survived
+notsurvived <- subset(sparr, Survivorship == 'NS')
+notsurvived
+
+#Calculating colMeans
+survived.num <- survived[,-1]
+colMeans(survived.num)
+
+notsurvived.num <- notsurvived[,-1]
+colMeans(notsurvived.num)
+
+#Creating Covariance matrices 
+cov(survived.num)
+cov(notsurvived.num)
+
+#Creating correlation matrices
+cor(survived.num)
+cor(notsurvived.num)
+
